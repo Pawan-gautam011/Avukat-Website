b76deec4405ab776cd863693af089560f9cc8218
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-09-16 00:29:06 +0545

    for twomorrow

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function myFunction() {
   }
 
 
+
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
